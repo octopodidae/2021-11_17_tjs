ca187c0d4efcc6d9cebc6237ab5334358d8b1e46
octopodidae <blaurancin@gmail.com>
2021-11-17 14:12:55 +0100

    creation du composant

---
+++ src/App/components/Button/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Button.css';
+
+
+function Button(props) {
+  return <button className="Button">Benjamin button</button>;
+}
+
+// const Button=(props)=>{
+//     return (
+//         <button className="Button">
+
+//         </button>
+//     );
+// }
